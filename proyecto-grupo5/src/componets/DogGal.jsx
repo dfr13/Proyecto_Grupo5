e5681e0f3fe55e991fe37d1b3606526239cb2b1b
MaxPrain12 <alder.velasquezobando@gmail.com>
2022-02-10 12:20:11 +0100

    Creo componente doggal

---
+++ proyecto-grupo5/src/componets/DogGal.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class DogGal extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return(
+            <div>
+                <h1>
+                    hola
+                </h1>
+            </div>
+        );
+    }
+}
+export default DogGal;
